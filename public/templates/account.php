30a29c30a3cc7863ed316f08c6e0cd9e0a5e2feb
berking-da-quan <ishimweeschylebersy@gmail.com>
2021-04-08 13:28:20 +0200

    adding new files

---
+++ public/templates/account.php
@@ -0,0 +1,71 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.82.0">
+    <title>Kgb Diary</title>
+    <!-- Bootstrap core CSS -->
+<link href="../css/app.css" rel="stylesheet">
+
+<meta name="theme-color" content="#7952b3">
+
+  </head>
+  <body>   
+    <nav>
+        <div class="nav-bar">
+            <div class="first-menu">
+              <div class="logo">
+                <a href="index.html">KGB Team</a>
+              </div>
+              <div class="nav-bar-link">
+                <a href="index.html">Home</a>
+                <a href="diary.html">Diary</a>
+                <a href="account.html">Account</a>
+              </div>
+            </div>
+            <div class="second-menu">
+              <div class="search_box">
+                <input type="search" name="search" class="search">
+                <button type="submit" class="submit-btn"><i class="fas fa-search"></i></span></button>
+              </div>
+            </div>
+    </nav>
+
+<main class="pega1">
+  <div class="Post_details">
+    <img class="profile_img" src="../img/j.png" alt="" width="150" height="150">
+    <div class="post_textes">
+      <h1>Pele Jean</h1>
+      <h3>Php motion developer</h3>
+      <h3>Web artisan on youtube</h3>
+      <h3>Django Master</h3>
+      <h3>Girl lover</h3>
+      <h3>@pelejean1995</h3>
+      
+    </div>
+    <div class="boutons">
+      <a href="">Modify</a>
+    </div>
+  </div>
+  
+
+
+  <div class="diary_interface">
+    <h1 class="title_di">Diary Poster Interface</h1>
+    <div class="interface">
+      <br>
+      <div class="wyswyg">@wyswyg import module....................</div>
+      <div class="borderf"></div>
+      <div class="boutons">
+        <a href="">Modify</a>
+        <a href="">Post</a>
+      </div>
+    </div>
+  </div>
+</main>
+  </body>
+</html>
---
+++ public/templates/home.php
@@ -0,0 +1,89 @@
+
+<body>
+  <nav>
+    <div class="nav-bar">
+      <div class="first-menu">
+        <div class="logo">
+          <a href="index.html">KGB Team</a>
+        </div>
+        <div class="nav-bar-link">
+          <a href="index.html">Home</a>
+          <a href="login.html">Account</a>
+        </div>
+      </div>
+      <div class="second-menu">
+        <div class="search_box">
+          <input type="search" name="search" class="search">
+          <button type="submit" class="submit-btn"><i class="fas fa-search"></i></span></button>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <section class="body-container">
+    <div class="kgb_members">
+      <div class="kgb_members_header">
+        <h2>our actual members</h2>
+      </div>
+      <div class="user">
+        <div class="user-data">
+          <img src="../img/j.png" alt="username - profile pic">
+          <div class="user-data_names">
+            <p class="fullname">full name</p>
+            <p class="username">@username</p>
+          </div>
+        </div>
+        <div class="link">
+          <a href="#">his diary</a>
+        </div>
+      </div>
+
+      <div class="user">
+        <div class="user-data">
+          <img src="../img/j.png" alt="username - profile pic">
+          <div class="user-data_names">
+            <p class="fullname">full name</p>
+            <p class="username">@username</p>
+          </div>
+        </div>
+        <div class="link">
+          <a href="#">his diary</a>
+        </div>
+      </div>
+
+      <div class="user">
+        <div class="user-data">
+          <img src="../img/j.png" alt="username - profile pic">
+          <div class="user-data_names">
+            <p class="fullname">full name</p>
+            <p class="username">@username</p>
+          </div>
+        </div>
+        <div class="link">
+          <a href="#">his diary</a>
+        </div>
+      </div>
+
+      <div class="user">
+        <div class="user-data">
+          <img src="../img/j.png" alt="username - profile pic">
+          <div class="user-data_names">
+            <p class="fullname">full name</p>
+            <p class="username">@username</p>
+          </div>
+        </div>
+        <div class="link">
+          <a href="#">his diary</a>
+        </div>
+      </div>
+
+
+    </div>
+    <div class="recent_post">
+      <h2>recent post from membersssss</h2>
+    </div>
+  </section>
+  <div class="footer">
+    <p>HGB Team copyright <span class="year"></span></p>
+  </div>
+  <script src="../js/app.js"></script>
+</body>
