8fe47d3cbd7461b5bfe174635dc84d05176ed27b
ISHIMWE Eschyle Bersy <ishimweeschylebersy@gmail.com>
2021-04-18 20:57:38 +0200

    writted the session manager class

--- src/helper/SessionManager.php
+++ src/helper/SessionManager.php
@@ -15,6 +15,71 @@ namespace src\helper;
 
 class SessionManager {
 
-    public $name;
+    /**
+     * 
+     * va nous permettre de creer des session
+     * 
+     * @param string $name le nom de la session
+     * @param string $value la valeur a utiliser
+     */
+
+    public function setSession($name, $value) : bool
+    {
+        if($_SESSION[$name] = $value) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    /**
+     * 
+     * va nous permettre d'avoir la valeur des sessions
+     * 
+     * @param string $name le nom de la session
+     * 
+     */
+
+
+    public function getSession($name): string
+    {
+        return $_SESSION[$name];
+    }
     
+
+    /**
+     * 
+     * va nous permettre de mettre a jour une session
+     * 
+     * @param string $name le nom de la session
+     * @param string $value la valeur a utiliser
+     */
+
+
+    public function updateSession($name, $value): bool
+    {
+        if($_SESSION[$name] = $value) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+
+    /**
+     * 
+     * va nous permettre de supprimer les sessions
+     * 
+     */
+    
+    public function deleteSession($name): bool
+    {
+        // remove all session variables
+        session_unset();
+        // destroy the session
+        session_destroy();
+
+        return true;
+    }
 }
