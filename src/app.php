93cd55bdddff1eaefe846243b4361868950a89d7
ISHIMWE Eschyle Bersy <ishimweeschylebersy@gmail.com>
2021-04-19 14:01:18 +0200

    created the save method for Note

--- src/app.php
+++ src/app.php
@@ -44,10 +44,31 @@ if(isset($action) && !empty($action)) {
                 $errors = 'username must contain min 5 caracters and max 20 caracters';
                 header('Location: ../register.php?errors='.$errors);
             }
-
             break;
+
         case "add_note":
-            echo "i work with notes...";
+            $validate = new Validator();
+            $_COOKIE['user_id'] = 4;
+            $user_obj = new User($db);
+            if(isset($_COOKIE['user_id'])) {
+                $user_obj = $user_obj->getOne($_COOKIE['user_id']);
+                while ($res = $user_obj->fetch(PDO::FETCH_ASSOC)) {
+                    extract($res);
+                    $user_id = $id;
+                }
+            } else {
+                echo "no user found!";
+            }
+            $note->title = $_POST['title'];
+            $note->user_id = $user_id;
+            $note->body = $_POST['body'];
+
+            if($note->save()) {
+                $message = 'successfuly created your note';
+                header('Location: ../account.php?m='.$message); }
+            else {
+                $error = 'failed to create your note';
+                header('Location: ../account.php?e='.$error); }
             break;
 
         case "update":
